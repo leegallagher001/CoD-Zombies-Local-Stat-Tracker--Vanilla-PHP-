2d13568a88e6c3932e06267e124bb92f63b57df4
leegallagher001 <lee.gallagher.writing@outlook.com>
2026-03-25 16:04:55 +0000

    Added templates for the other pages to make things easier when I start work on those.

---
+++ add-record.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CoD Zombies Stat Tracker - Home</title>
+    <link rel="stylesheet" href="CSS/main.css">
+</head>
+
+<body>
+
+    <header>
+        <a id="header-link" href="index.php"><h1>CoD Zombies - Local Stat Tracker</h1></a>
+    </header>
+
+    <nav>
+        <div id="nav-menu">
+            <a href="add-record.php">Add Match</a>
+            <a href="view-records.php">View All Match Records</a>
+            <a href="search-records.php">Search Matches</a>
+            <a href="delete-record.php">Delete Matches</a>
+        </div>
+    </nav>
+
+    <main>
+
+    <h2>UNDER CONSTRUCTION</h2>
+
+    </main>
+
+    <footer>
+        <?php
+        echo "<h3>Created by Lee Gallagher DDCC &copy; " . date("Y") . "</h3>"
+        ?>
+    </footer>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -10,15 +10,15 @@
 <body>
 
     <header>
-        <h1>CoD Zombies - Local Stat Tracker</h1>
+        <a id="header-link" href="index.php"><h1>CoD Zombies - Local Stat Tracker</h1></a>
     </header>
 
     <nav>
         <div id="nav-menu">
-            <a href="#">Add Match</a>
-            <a href="#">View All Match Records</a>
-            <a href="#">Search Matches</a>
-            <a href="#">Delete Matches</a>
+            <a href="add-record.php">Add Match</a>
+            <a href="view-records.php">View All Match Records</a>
+            <a href="search-records.php">Search Matches</a>
+            <a href="delete-record.php">Delete Matches</a>
         </div>
     </nav>
 
@@ -30,28 +30,28 @@
             <p>This project aims to build a local stat tracker for anyone who plays a lot of the old Call of Duty zombies games in offline single-player or split-screen modes.</p>
         </div>
 
-        <a id="new" href="#">
+        <a id="new" href="add-record-php">
             <div class="home-page-link">
                 <h2>"The Mystery Box" - ADD RECORD</h2>
                 <p>Add a record of a recent single-player or split-screen zombies match.</p>
             </div>
         </a>
 
-        <a id="view-all" href="#">
+        <a id="view-all" href="view-records.php">
             <div class="home-page-link">
                 <h2>"Fetch Me Their Souls" - VIEW MATCH RECORDS</h2>
                 <p>View all records of all matches recorded so far.</p>
             </div>
         </a>
 
-        <a id="search" href="#">
+        <a id="search" href="search-records.php">
             <div class="home-page-link">
                 <h2>"Easter Egg Hunt" - SEARCH RECORDS</h2>
                 <p>Look here to search for specifics, such as specific map stats, round records or specific player stats.</p>
             </div>
         </a>
 
-        <a id="delete" href="#">
+        <a id="delete" href="delete-record.php">
             <div class="home-page-link">
                 <h2>"Bye Bye" - DELETE RECORD</h2>
                 <p>Mainly for the purposes of setting a record straight in the event of a mistake made in entry, records can be deleted here.</p>
