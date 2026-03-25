c04f36d62e01ae75a19fea706249b14e397caf13
leegallagher001 <lee.gallagher.writing@outlook.com>
2026-03-25 15:23:39 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CoD Zombies Stat Tracker - Home</title>
+    <link rel="stylesheet" href="CSS/main.css">
+</head>
+<body>
+
+    <header>
+        <h1>CoD Zombies - Local Stat Tracker</h1>
+    </header>
+
+    <nav>
+        <div id="nav-menu">
+            <a href="#">Add Match</a>
+            <a href="#">View All Match Records</a>
+            <a href="#">Search Matches</a>
+            <a href="#">Delete Matches</a>
+        </div>
+    </nav>
+
+    <main>
+        <h2>Home</h2>
+        <p>Hello, and welcome to the Call of Duty Zombies local stat tracker!</p>
+        <p>This project aims to build a local stat tracker for anyone who plays a lot of the old Call of Duty zombies games in offline single-player or split-screen modes.</p>
+    </main>
+
+    <footer>
+        <h3>Created by Lee Gallagher DDCC &copy; <?php date("Y")?></h3>
+    </footer>
+
+</body>
+</html>
